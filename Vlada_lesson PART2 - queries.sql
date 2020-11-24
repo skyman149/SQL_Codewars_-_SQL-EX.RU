b2e7142d3e8e0d9ed3bff0a7a56a56b8e8c0ad6d
Anton <52634903+skyman149@users.noreply.github.com>
2020-11-23 18:46:31 -0800

    Update Vlada_lesson PART2 - queries.sql

--- Vlada_lesson PART2 - queries.sql
+++ Vlada_lesson PART2 - queries.sql
@@ -22,6 +22,43 @@ SELECT * FROM country
 WHERE country_name IN ('Latvia', 'Lithuania', 'Estonia');
 /*Selects all info related to provided counties*/
 
+SELECT * FROM country
+WHERE population BETWEEN 1000000 AND 5000000;
+/*inclusive both numbers*/
+SELECT * FROM country
+WHERE population > 1000000 AND 
+	  population < 5000000;
+/*not inclusive both numbers*/
+
+SELECT * FROM user
+WHERE state IS NULL;
+/* IS used only with NULL*/
+SELECT * FROM user
+WHERE state != 'CA' OR state IS NULL;
+/*state not CA or state not provided*/
+SELECT * FROM user
+WHERE IS NOT NULL;
+/*only if state info provided*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
