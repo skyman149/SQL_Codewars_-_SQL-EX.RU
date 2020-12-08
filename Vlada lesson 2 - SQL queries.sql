ef80f015f3dc6b9895a2b52a0d1338196cd6749c
Anton <52634903+skyman149@users.noreply.github.com>
2020-12-07 17:35:21 -0800

    Update Vlada lesson 2 - SQL queries.sql

--- Vlada lesson 2 - SQL queries.sql
+++ Vlada lesson 2 - SQL queries.sql
@@ -1,3 +1,6 @@
+
+/* ------------- userr -> user ---------------- */
+
 /* 3 */
 SELECT 
 	country_name AS "Country Name",
@@ -6,11 +9,11 @@ FROM country
 WHERE population >= 1000000 AND population <= 10000000;
 
 /* 4. Select all users with last names that have 'a' as a second letter. */
-SELECT * FROM user 
+SELECT * FROM userr 
 WHERE lats_name LIKE "_a%"; /* _ -> skip first letter, % -> any number of letters*/
 
 /* 5. select all users from Kiev, Vancuver, Moscow*/
-SELECT * FROM user
+SELECT * FROM userr
 WHERE city NOT IN ('Moscow', 'Kiev');
 
 /* select all counteis where country name starting with 'B' and pop > 9000000*/
@@ -27,7 +30,8 @@ WHERE country_name LIKE '%s'
 ORDER BY country_name;
 
 /**/
-SELECT * FROM user
+SELECT * 
+FROM userr
 WHERE state IN ('CA', 'NV') AND rank > 100;
 
 /*3 colums will be shown, math operators +,-,*,/ can be used*/
@@ -41,17 +45,25 @@ SELECT * FROM country
 WHERE population_2019 - population_2018 = 1;
 
 SELECT first_name, email, rank
-FROM user
+FROM userr
 WHERE state = 'CA' AND
 	  rank > 200;
       
-SELECT *, population_2019 - population_2018 AS Population growth per year
+SELECT *, population_2019 - population_2018 AS Population_growth_per_year
 FROM country
 WHERE country_name > 'B' 
 AND population_2019 - population_2018 > 1000
 ORDER BY country_name;
 
 /*  DISTINCT  |  LIMIT  */
+
+SELECT * 
+FROM country
+LIMIT /*10*/; /* Only 10 countries will be shown */
+
+SELECT DISTINCT * 
+FROM country; /* only unique data will be shown */
+
 
 
 
