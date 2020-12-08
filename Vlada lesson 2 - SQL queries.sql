618393be3b971b51b16c83646d7fcbf260d2291a
Anton <52634903+skyman149@users.noreply.github.com>
2020-12-07 16:40:48 -0800

    Update Vlada lesson 2 - SQL queries.sql

--- Vlada lesson 2 - SQL queries.sql
+++ Vlada lesson 2 - SQL queries.sql
@@ -7,4 +7,11 @@ WHERE population >= 1000000 AND population <= 10000000;
 
 /* 4. Select all users with last names that have 'a' as a second letter. */
 SELECT * FROM user 
-WHERE lats_name LIKE "_a%"
+WHERE lats_name LIKE "_a%"; /* _ -> skip first letter, % -> any number of letters*/
+
+/* 5. select all users from Kiev, Vancuver, Moscow*/
+SELECT * FROM user
+WHERE city NOT IN ('Moscow', 'Kiev');
+
+
+
