e20c47df0293f440c8d12c42699e23c9abcc96ff
Anton <52634903+skyman149@users.noreply.github.com>
2020-12-10 17:00:59 -0800

    Update Vlada_lesson 3 - SQL SELECT WHERE AND OR.sql

--- Vlada_lesson 3 - SQL SELECT WHERE AND OR.sql
+++ Vlada_lesson 3 - SQL SELECT WHERE AND OR.sql
@@ -16,7 +16,32 @@
 --         user.*
 -- FROM user;
 
--- Z-A
-SELECT *
-FROM country
-ORDER BY country_name DESC;
+--  Z-A
+-- SELECT *
+-- FROM country
+-- ORDER BY country_name DESC;
+
+
+--  specific info (in this case Moscow) -- != or <> - not equal
+-- SELECT *
+-- FROM user
+-- WHERE city = 'Moscow';
+
+-- SELECT *
+-- FROM user
+-- WHERE rank > 100;
+
+-- SELECT rank, city
+-- FROM user
+-- WHERE rank >= 100 OR
+--       city = 'Las Vegas';
+
+-- SELECT *
+-- FROM country
+-- WHERE population_2019 > 1000000 OR 
+--       id < 20
+-- ORDER BY population_2019;
+
+
+
+
