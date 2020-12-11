36cb5051ac341ae6b75645091662bde315d36613
Anton <52634903+skyman149@users.noreply.github.com>
2020-12-10 16:33:09 -0800

    Update Vlada_lesson 3 - SQL SELECT WHERE AND OR.sql

---
+++ Vlada_lesson 3 - SQL SELECT WHERE AND OR.sql
@@ -0,0 +1,19 @@
+-- SELECT  id,
+-- 		   first_name,
+--         last_name,
+--         rank
+-- FROM user;
+
+--                          OR SAME THING
+
+-- SELECT  user.id,
+-- 		   user.first_name,
+--         user.last_name,
+--         user.rank
+-- FROM user;
+
+-- if you want specific column to be first (for comfort) -- user.* -> just because, where user is table name
+-- SELECT  rank, 
+--         user.*
+-- FROM user;
+
