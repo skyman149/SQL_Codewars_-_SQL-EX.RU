dea8d1ff906cbf5b63ad79ad9bda3ed96cd6cf44
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-28 12:12:07 -0800

    Update Vlada_lesson 5 - JOIN.sql

--- Vlada_lesson 5 - JOIN.sql
+++ Vlada_lesson 5 - JOIN.sql
@@ -1,2 +1,7 @@
 -- https://www.db-fiddle.com/f/u55yPYz7QbEGdpJPyE5BHs/0
 
+SELECT city.city_name, country.country_name_eng
+FROM city
+LEFT JOIN country ON city.country_id = country.id
+WHERE country.country_name_eng = 'Germany';
+
