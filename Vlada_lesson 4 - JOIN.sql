9ebd44189f182eefa6b84a38aad966ff77827c43
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-27 20:09:14 -0800

    Update Vlada_lesson 4 - JOIN.sql

--- Vlada_lesson 4 - JOIN.sql
+++ Vlada_lesson 4 - JOIN.sql
@@ -1,5 +1,7 @@
 -- see database calls in Vlada_lesson 1.0
 
+-- https://www.db-fiddle.com/f/vTEGCb6tg182pUbcWrbeVY/1
+
 -- group by year, month sum of all inbound calls
 SELECT  year,
 		month,
@@ -17,3 +19,14 @@ GROUP BY year,
          month
 HAVING Total_per_month > 100
 ORDER BY total_per_month DESC;
+
+--   INNER JOIN / JOIN
+
+-- SELECT * 
+-- FROM country
+-- JOIN user ON country.id = user.country_id;
+
+-- SELECT country_name, first_name, last_name
+-- FROM country
+-- JOIN user ON country.id = user.country_id;
+
