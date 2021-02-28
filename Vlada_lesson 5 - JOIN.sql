d23501c394cb15a4c1997fe01583cb4713bda61d
Anton <52634903+skyman149@users.noreply.github.com>
2021-02-28 12:21:16 -0800

    Update Vlada_lesson 5 - JOIN.sql

--- Vlada_lesson 5 - JOIN.sql
+++ Vlada_lesson 5 - JOIN.sql
@@ -5,3 +5,8 @@ FROM city
 LEFT JOIN country ON city.country_id = country.id
 WHERE country.country_name_eng = 'Germany';
 
+
+SELECT customer.customer_name AS "Customer Name", 
+	   city.city_name AS "Customer City"
+FROM customer
+LEFT JOIN city ON customer.id = city.id;
